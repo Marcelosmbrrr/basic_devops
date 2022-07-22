0a1313bf1a7f4b63265d1f75f9eb067fdedfd3ce
Marcelo Moreira <profissionalsmbr@gmail.com>
2022-07-22 01:58:32 -0300

    test with nginx and php

---
+++ 2_Containers_with_Docker/testing/nginx_php_mysql/www/index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Server is on!";
+
+/*$host = "mysql_db";
+$database = "shop";
+$user = "root";
+$pass = "root";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$database", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "<h1>Successful connection</h1>";
+} catch(PDOException $e){
+    echo 'ERROR: ' . $e->getMessage();
+}
+*/
